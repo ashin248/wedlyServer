e19451fedab9dfbfc815e83f03436003fdd3a109
ashin248 <ashinbiju248@gmail.com>
2025-09-20 17:28:42 +0530

    update server

--- Auth/AdminAuth.js
+++ Auth/AdminAuth.js
@@ -1,20 +1,10 @@
-// const AdminAuth = (req, res, next) => {
-//     if (!req.session || !req.session.admin) {
-//         console.log('Admin authentication failed: No session or admin data');
-//         return res.status(401).json({ message: 'Unauthorized: Admin access required' });
-//     }
-//     next();
-// };
-
-// module.exports = AdminAuth;
-
-
-const checkAuth = (req, res, next) => {
-    if (!req.session || !req.session.user) {
-        console.log('Authentication failed: No session or user data');
-        return res.status(401).json({ message: 'Unauthorized' });
+const AdminAuth = (req, res, next) => {
+    if (!req.session || !req.session.admin) {
+        console.log('Admin authentication failed: No session or admin data');
+        return res.status(401).json({ message: 'Unauthorized: Admin access required' });
     }
     next();
 };
 
-module.exports = checkAuth;
+module.exports = AdminAuth;
+
--- Auth/user.js
+++ Auth/user.js
@@ -25,3 +25,4 @@ router.get('/', checkAuth, async (req, res) => {
 });
 
 module.exports = router;
+
